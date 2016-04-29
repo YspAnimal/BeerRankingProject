7b825ee5c9799d2313cd158b038087f950b86735
YspAnimal <Animal1986@mail.ru>
2016-04-29 16:10:33 +0400

    1

--- WordAnalyzeModule.R
+++ WordAnalyzeModule.R
@@ -1,12 +1,13 @@
 require(sqldf)
 require(RSQLite)
+require(tm)
+require(dplyr)
 
 
 db <- dbConnect(SQLite(), dbname="BeerDB.sqlite")
 BeerStyles <- dbReadTable(db, "Styles")
 Beers <- dbReadTable(db, "Beers")
 GeneralInfo <- dbReadTable(db, "GeneralInfo")
-#
 
 SQLQuery <- "SELECT style, Name, Beers.BeerLink, Description FROM Beers JOIN GeneralInfo
                 ON (Beers.BeerLink = GeneralInfo.BeerLink)"
@@ -15,5 +16,7 @@ SQLQuery <- "SELECT style, Name, Beers.BeerLink, Description FROM Beers JOIN Gen
 #WHERE Beers.BeerLink = 71"
 
 myQuery <- dbSendQuery(db, SQLQuery)
-my_data <- dbFetch(myQuery, n = -1)
+Beerdata <- dbFetch(myQuery, n = -1)
 dbDisconnect(db)
+
+temp <- filter(Beerdata, style == "71") %>% select(Description)
